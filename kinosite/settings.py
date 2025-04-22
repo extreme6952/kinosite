5104975a8fd8f6136546fa7a19e3a1de33c95ae9
extreme6952 <kaznacheev1300@mail.ru>
2025-04-22 21:05:08 +0300

    Сделал конект в postgresql бд, сделал в utf-8 код.

--- kinosite/settings.py
+++ kinosite/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'kinosite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kinosite_database',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
