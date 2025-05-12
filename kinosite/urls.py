1654b50ce869490acbb660a405cc23f17ac84df2
extreme6952 <kaznacheev1300@mail.ru>
2025-05-12 08:08:05 +0300

    Просто обновил список пакетов и задач

--- kinosite/urls.py
+++ kinosite/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('series-users/',include('cinema.urls')),
+    path('',include('cinema.urls')),
     path('accounts/',include('accounts.urls',namespace='account')),
     # path('captcha/',include('captcha.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
